b5a1fcea75b9557c8e6ae9514f963432f303895e
Krisztian Nagy <chrisakakay@gmail.com>
2025-05-16 11:50:27 +0200

    todo highlights

--- .config/nvim/lua/chrisakakay/core/keymaps.lua
+++ .config/nvim/lua/chrisakakay/core/keymaps.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- window management
-
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
@@ -15,6 +14,10 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- move lines
+keymap.set("n", "<C-S-Down>", ":m +1<CR>", { desc = "Move line down" })
+keymap.set("n", "<C-S-Up>", ":m -2<CR>", { desc = "Move line up" })
 
 -- project management
 -- keymap.set("n", "<leader>pp", "<cmd>Ex<CR>", { desc = "Show file navigator" })
